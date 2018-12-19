6fd57c61602861bbe5c70e74ef3a2db62e5a3260
hossain-rayhan <hossain.rayhan@outlook.com>
2018-12-19 18:00:07 -0500

    CompareTheTriplest and AVeryBigSum solved

---
+++ CompareTheTriplets.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+            int a_score = 0, b_score = 0;
+            for(int i = 0; i < 3; i++){
+                if(a.get(i) > b.get(i))
+                    a_score++;
+                else if(b.get(i) > a.get(i))
+                    b_score++;
+            }
+        List<Integer> result_list = new ArrayList<Integer>();
+        result_list.add(a_score);
+        result_list.add(b_score);
+        return result_list;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
