2064c918d6ddea758fa54897d5fad1878b108f20
hossain-rayhan <hossain.rayhan@outlook.com>
2018-12-21 21:23:35 -0500

    Array Manipulation Solved from Interview Kit

---
+++ ArrayManipulation.java
@@ -0,0 +1,63 @@
+mport java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries) {
+        long [] a = new long[n+1];
+
+        for(int j = 0; j < queries.length; j++){
+            a[queries[j][0]] += queries[j][2];
+            if(queries[j][1] + 1 < n+1)
+                a[queries[j][1] + 1] -= queries[j][2];
+        }
+        long max = 0, x = 0;
+        for(int i = 0; i < n+1; i++){
+            x += a[i];
+            if(x > max)
+                max = x;
+        }
+
+        return max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[][] queries = new int[m][3];
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowItems[j]);
+                queries[i][j] = queriesItem;
+            }
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
