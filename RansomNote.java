3111bf581867a0675964f5e4f487e95e6a650590
hossain-rayhan <hossain.rayhan@outlook.com>
2018-12-26 19:34:12 -0500

    Hash Tables: Ransom Note solved from Interview Kit

---
+++ RansomNote.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+        if(magazine.length < note.length){
+            System.out.println("No");
+            return;
+        }
+        List<String> list = new ArrayList<>();
+        for(int i = 0; i < magazine.length; i++){
+            list.add(magazine[i]);
+        }
+        for(int i = 0; i < note.length; i++){
+            if(!list.remove(note[i])){
+               System.out.println("No");
+               return; 
+            }
+        }
+        System.out.println("Yes");
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        String[] magazine = new String[m];
+
+        String[] magazineItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            String magazineItem = magazineItems[i];
+            magazine[i] = magazineItem;
+        }
+
+        String[] note = new String[n];
+
+        String[] noteItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            String noteItem = noteItems[i];
+            note[i] = noteItem;
+        }
+
+        checkMagazine(magazine, note);
+
+        scanner.close();
+    }
+}
+
