ac8f6a29c5a6a3477cd8d4e4a53f2e5163bb942b
hossain-rayhan <hossain.rayhan@outlook.com>
2019-01-22 00:36:14 -0500

    Insertion Sort - Part 1 solved from Sorting

---
+++ InsertionSortPart1.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the insertionSort1 function below.
+    static void insertionSort1(int n, int[] arr) {
+        int hole = n - 1;
+        int value = arr[hole];
+        while(hole > 0 && arr[hole-1] > value){
+            arr[hole] = arr[hole-1];
+            hole--;
+            printArray(arr);
+        }
+        arr[hole] = value;
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        insertionSort1(n, arr);
+
+        scanner.close();
+    }
+}
+
